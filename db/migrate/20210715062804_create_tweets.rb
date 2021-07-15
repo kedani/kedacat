31f73288f3f22dbbccb26b36dbadd5f0cfcbccef
kedani <75819419+kedani@users.noreply.github.com>
2021-07-16 00:17:54 +0900

    テスト

--- db/migrate/20210715062804_create_tweets.rb
+++ db/migrate/20210715062804_create_tweets.rb
@@ -1,7 +1,15 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string     :back,           null: false
+      t.string     :personal,       null: false
+      t.string     :health,         null: false
+      t.string     :method,         null: false
+      t.string     :image,          null: false
+      # t.string     :status,         null: false
+      # t.integer    :prefecture_id,  null: false
+      # t.string     :buyer_id
+      # t.references :category,       null: false
       t.timestamps
     end
   end
